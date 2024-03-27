ae0c7e655cec9e4c15d82a4853b7ea5461777b6c
Arthur Fernandes <arthur_sbf@outlook.com>
2024-03-26 21:13:19 -0300

    apostila (git-jvaa):atualziação nas linhas s do código da aula Please enter the commit message for your changes. Lines starting

--- Calculadora.java
+++ Calculadora.java
@@ -1,8 +1,22 @@
 public class Calculadora {
+
+    private double meuNumeroPreferido;
+
+    public Calculadora(double meuNumeroPreferido) {
+        this.meuNumeroPreferido = meuNumeroPreferido;
+    }
+
     public double somar(double a, double b){
         return a + b;
     }
     public double subtrair (double a, double b){
         return a - b;
+    }
+
+    public double somarComPreferido(double a) {
+        return a + meuNumeroPreferido;
+    }
+    public double subtrairComPreferido(double a) {
+        return a - meuNumeroPreferido;
     }
 }
--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -1,11 +1,16 @@
+import javax.swing.JOptionPane;
+
 public class CalculadoraTeste {
-    public static void main(String[] args) {
-        Calculadora calc = new Calculadora();
+     public static void main(String[] args) {
+        Calculadora calc = new Calculadora(8);
 
         double somar = calc.somar(2, 3);
         System.out.println(somar);
 
         double subtracao = calc.subtrair(2, 3);
         System.out.println(subtracao);
+
+        System.out.println(calc.somarComPreferido);
+        System.out.println(calc.subtrairComPreferido);
     }
 }
